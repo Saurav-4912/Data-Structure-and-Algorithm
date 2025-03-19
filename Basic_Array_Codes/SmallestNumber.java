ab83215d30e4f353d6bd0626f894ba97e086315b
Saurav Pawar <sauravpawar46@gmail.com>
2025-03-19 13:01:22 +0530

    Find the smallest number in an array

---
+++ Basic_Array_Codes/SmallestNumber.java
@@ -0,0 +1,16 @@
+public class SmallestNumber {
+  public static void main(String[] args) {
+    System.out.println("Hello World ");
+
+    int[] arr = {42, 7, 19, 85, 3, 61, 27, 14};
+
+    int num = arr[0];
+    for(int i = 1; i<arr.length ; i++){
+      if(arr[i] < num){
+        num = arr[i];
+      }
+    }
+
+    System.out.println("Smallest Number : " + num);
+  }
+}
