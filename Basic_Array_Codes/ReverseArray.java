e37292b4166b21c22ead115d6d4c4fd6028d3866
Saurav Pawar <sauravpawar46@gmail.com>
2025-03-23 12:36:20 +0530

    Reverse Array Updated Code

--- Basic_Array_Codes/ReverseArray.java
+++ Basic_Array_Codes/ReverseArray.java
@@ -2,10 +2,13 @@ package Basic_Array_Codes;
 
 public class ReverseArray {
   public static void main(String[] args) {
-    int[] arr = {42, 7, 19, 85, 3, 61, 27, 14};
+    int[] arr = { 42, 7, 19, 85, 3, 61, 27, 14 };
 
-    for(int i = arr.length-1 ; i >= 0 ; i--){
+    System.out.print("[");
+    for (int i = arr.length - 1; i >= 0; i--) {
       System.out.print(arr[i] + " ");
     }
+
+    System.out.println("]");
   }
 }
