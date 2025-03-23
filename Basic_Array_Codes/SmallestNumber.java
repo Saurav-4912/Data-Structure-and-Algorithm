916c5ce481e6c7e417c16b4546f948288a796162
Saurav Pawar <sauravpawar46@gmail.com>
2025-03-23 12:29:13 +0530

    Smallest Number in array

--- Basic_Array_Codes/SmallestNumber.java
+++ Basic_Array_Codes/SmallestNumber.java
@@ -1,9 +1,8 @@
-
 public class SmallestNumber {
   public static void main(String[] args) {
     System.out.println("Hello World ");
 
-    int[] arr = {42, 7, 19, 85, 3, 61, 27, 14};
+    int[] arr = {99, 0, 99, 100};
 
     int num = arr[0];
     for(int i = 1; i<arr.length ; i++){
