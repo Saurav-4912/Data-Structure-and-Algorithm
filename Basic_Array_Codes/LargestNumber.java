70c8f0fa73d319b0e1ce0d30fbbc54bd4a50a750
Saurav Pawar <sauravpawar46@gmail.com>
2025-03-23 12:31:14 +0530

    Largest Number in array (Updated Code)

--- Basic_Array_Codes/LargestNumber.java
+++ Basic_Array_Codes/LargestNumber.java
@@ -2,7 +2,7 @@ package Basic_Array_Codes;
 
 public class LargestNumber {
   public static void main(String[] args) {
-    int[] arr = {42, 7, 19, 85, 3, 61, 27, 14};
+    int[] arr = {-3, -5, -1, -10};
 
     int largestNumber = arr[0];
     for(int i = 1 ; i < arr.length ; i++){
