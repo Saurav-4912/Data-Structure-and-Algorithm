705db85b9de94517f39e7200dafa8bb56b916502
Saurav Pawar <sauravpawar46@gmail.com>
2025-04-07 20:59:30 +0530

    Removed resources folder and Student.ser

---
+++ Serilization_Deserilization/Demo.java
@@ -0,0 +1,35 @@
+package Serilization_Deserilization;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class Demo {
+
+  public static void main(String[] args) {
+
+    ObjectOutputStream oos = null;
+    FileOutputStream fos = null;
+
+    try {
+
+      fos = new FileOutputStream("student.ser");
+      oos = new ObjectOutputStream(fos);
+
+      Student student = new Student(101,"AAA",78);
+      oos.writeObject(student);
+
+      System.out.println("Object is serilized successfully");
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+
+      try {
+        oos.close();
+        fos.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
