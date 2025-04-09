239ba2d7be1cd3f0b35d6c678574382f7e094c61
Saurav Pawar <sauravpawar46@gmail.com>
2025-04-09 15:18:44 +0530

    Added Previous Codes

---
+++ Serilization_Deserilization/Student.java
@@ -0,0 +1,49 @@
+package Serilization_Deserilization;
+
+import java.io.Serializable;
+
+class Student implements Serializable{
+
+  private static final long serialVersionUID = 1L;
+  
+  private int rno;
+  private String name;
+  private double per;
+
+  public Student() {
+  }
+
+  public Student(int rno , String name , double per){
+    this.rno = rno;
+    this.name= name;
+    this.per = per;
+  }
+  public int getRno() {
+    return rno;
+  }
+  public void setRno(int rno) {
+    this.rno = rno;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public double getPer() {
+    return per;
+  }
+  public void setPer(double per) {
+    this.per = per;
+  }
+
+  public void display(){
+    System.out.println("RNO : " + rno);
+    System.out.println("NAME : " + name);
+    System.out.println("PER : " + per);
+  }
+  @Override
+  public String toString() {
+    return "Student [rno=" + rno + ", name=" + name + ", per=" + per + "]";
+  }
+}
