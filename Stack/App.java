f8a4328330bfa30ede151be903e6f4dd7196fdd5
Saurav Pawar <sauravpawar46@gmail.com>
2025-05-08 08:03:55 +0530

    I have added isFull() and isEmpty() method in staticStack class

--- Stack/App.java
+++ Stack/App.java
@@ -3,6 +3,12 @@ package Stack;
 public class App {
   public static void main(String[] args) {
     
-    
+    StaticStack st = new StaticStack(10);
+
+    boolean status = st.isEmpty();
+    System.out.println("Is Empty : " + status);
+
+    boolean status1 = st.isFull();
+    System.out.println("Is Full : " + status1);
   }
 }
--- Stack/StaticStack.java
+++ Stack/StaticStack.java
@@ -1,5 +1,24 @@
 package Stack;
 
 public class StaticStack {
-  
+  int stack[];
+  int top;
+  int capacity;
+
+  public StaticStack(int capacity){
+    if(capacity < 1)
+    throw new IllegalArgumentException("Capacity should be greater than 0 ");
+
+    this.capacity =capacity;
+    stack = new int[capacity];
+    top = -1;
+  }
+
+  public boolean isEmpty(){
+    return top == -1;
+  }
+
+  public boolean isFull(){
+    return top == capacity-1;
+  }
 }
