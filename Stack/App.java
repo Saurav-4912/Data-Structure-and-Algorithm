efe6b0a660e2e4eab321a2e74f7c549ca93a32ca
Saurav Pawar <sauravpawar46@gmail.com>
2025-05-13 13:22:52 +0530

    Testing Code

--- Stack/App.java
+++ Stack/App.java
@@ -10,5 +10,20 @@ public class App {
 
     boolean status1 = st.isFull();
     System.out.println("Is Full : " + status1);
+
+    st.push(11);
+    st.push(22);
+    st.push(33);
+    st.push(44);
+    st.push(55);
+
+    System.out.println(st);
+
+    System.out.println("Size of Stack : " + st.size());
+
+    System.out.println("Popped Data : " + st.pop());
+
+    System.out.println("Peek Data : " + st.peek());
+    
   }
 }
