394598fa74459a8363031f4f65b2e7aeae88f6da
Saurav Pawar <sauravpawar46@gmail.com>
2025-05-16 14:30:40 +0530

    I have check all function in Test.java class

--- Queue/Test.java
+++ Queue/Test.java
@@ -2,8 +2,22 @@ package Queue;
 
 public class Test {
   public static void main(String[] args) {
-    StaticQueue sq = new StaticQueue(5);
+   StaticQueue sq = new StaticQueue(4);
 
-    System.out.println("Is Empty : " + sq.isEmpty());
+		sq.add(11);
+		sq.add(22);
+		sq.add(33);
+		System.out.println("Offer : " + sq.offer(44));
+
+		System.out.println(sq);
+		
+		System.out.println("Peek : " + sq.peek());
+
+		System.out.println("Remove : " + sq.remove());
+		System.out.println("Poll : " + sq.poll());
+		System.out.println("Poll : " + sq.poll());
+		System.out.println("Remove : " + sq.remove());
+		
+		System.out.println("Is Empty : " + sq.isEmpty());
   }
 }
