ac3292cd0ed94932d347be62d96bdd76c9045861
Saurav Pawar <sauravpawar46@gmail.com>
2025-05-25 11:46:53 +0530

    Added MyDeque class and structure of class using linkedlist

---
+++ Queue/MyDeque.java
@@ -0,0 +1,26 @@
+package Queue;
+
+public class MyDeque {
+  private Node first;
+	private Node last;
+	private int count;
+
+	public MyDeque() {
+		this.first = null;
+		this.last = null;
+		this.count = 0;
+	}
+
+	private static class Node {
+		private Node prev;
+		private int data;
+		private Node next;
+
+		public Node(int data) {
+			this.prev = null;
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+}
