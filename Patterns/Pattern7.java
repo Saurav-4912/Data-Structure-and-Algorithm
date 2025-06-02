780e3933d1fcf5d81035dd267d712b8435457e24
Saurav Pawar <sauravpawar46@gmail.com>
2025-06-02 09:18:37 +0530

    Added Program number 7

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -1,17 +1,15 @@
-public class Pattern6 {
+public class Pattern7 {
   public static void main(String[] args) {
-
+    
     int n = 4;
-    printRightAngleTriangle(n);
+    printSquare(n);
   }
-
-  
-	public static void printRightAngleTriangle(int n)
+  	public static void printSquare(int n)
 	{
 		for(int i=1; i<=n; i++)
-		{
+		{ 
 			
-			for(int j=1; j<=i; j++)
+			for(int j=1; j<=n; j++)
 			{
 				System.out.print("*"); 
 			}
@@ -20,7 +18,5 @@ public class Pattern6 {
 		}
 		System.out.println("-----------------------");
 	}
-	
-
 
 }
