05350102c66cb88140e711502f5b1d23ac717d1d
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-21 16:27:27 +0530

    Added reverse string code

--- LogicQuestions_40/ReverseString.java
+++ LogicQuestions_40/ReverseString.java
@@ -6,12 +6,25 @@ public class ReverseString {
 
   public static void main(String[] args) {
     String str = "saurav";
-
+/* 
     char[] character = str.toCharArray();
 
     for (int i = character.length - 1; i >= 0; i--) {
       System.out.println(character[i]);
     }
+  */
+
+   Stack<Character> stack = new Stack<>();
+
+   for(Character ch : str.toCharArray()){
+    stack.push(ch);
+   }
+
+   while(!stack.isEmpty()){
+    System.out.println(stack.pop());
+   }
+
+
   }
 
 }
