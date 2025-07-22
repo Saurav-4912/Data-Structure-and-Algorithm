7229b47bb8e4cecc93da54235ca9be86a5d76e69
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-22 15:57:27 +0530

    Rermove Duplicate From Array

--- LogicQuestions_40/RemoveDuplicate.java
+++ LogicQuestions_40/RemoveDuplicate.java
@@ -4,15 +4,36 @@ import java.util.LinkedHashSet;
 
 public class RemoveDuplicate {
   public static void main(String[] args) {
-    int[] arr = {1,2,2,3,4,4};
 
-    LinkedHashSet<Integer> L = new LinkedHashSet<>();
+    // LinkedHashSet<Integer> L = new LinkedHashSet<>();
 
-    for(int num : arr){
-      L.add(num);
+    // for (int num : arr) {
+    // L.add(num);
+    // }
+    // System.out.println(L);
+
+    int[] arr = { 1, 2, 2, 3, 4 };
+
+    int[] arr1 = new int[arr.length];
+
+    int index = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+
+      boolean isDuplicate = false;
+      for (int j = 0; j < index; j++) {
+        if (arr[i] == arr1[j]) {
+          isDuplicate = true;
+          break;
+        }
+      }
+      if (!isDuplicate) {
+        arr1[index] = arr[i];
+        index++;
+      }
     }
-
-    System.out.println(L);
-    
+    for (int i = 0; i < index; i++) {
+      System.out.print(arr1[i] + " ");
+    }
   }
 }
