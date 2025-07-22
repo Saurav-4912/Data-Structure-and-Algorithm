c47e2fc3ea33ab36c81c43a82cbbb5ed06556dc5
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-22 15:35:50 +0530

    Sort array by ascending order using inbuild method

--- LogicQuestions_40/SortArray.java
+++ LogicQuestions_40/SortArray.java
@@ -1,20 +1,31 @@
 package LogicQuestions_40;
 
+import java.util.Arrays;
+
 public class SortArray {
   public static void main(String[] args) {
     int[] arr = { 5, 2, 8, 1, 3 };
+    /*
+     * // Bubble sort logic
+     * 
+     * for(int i = 0; i<arr.length; i++){
+     * for(int j = i+1; j<arr.length; j++){
+     * if(arr[j] > arr[i]){
+     * int temp = arr[i];
+     * arr[i] = arr[j];
+     * arr[j] = temp;
+     * }
+     * }
+     * }
+     * for (int i = 0; i < arr.length; i++) {
+     * System.out.println(arr[i]);
+     * }
+     */
 
-    // Bubble sort logic
+    // Another Approch
 
-    for(int i = 0; i<arr.length; i++){
-      for(int j = i+1; j<arr.length; j++){
-        if(arr[j] > arr[i]){
-          int temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
-        }
-      }
-    }
+    Arrays.sort(arr);
+
     for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i]);
     }
