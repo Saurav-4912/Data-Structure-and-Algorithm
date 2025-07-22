c89257111dc874ed6c009198e18d2dc95b31193b
Saurav Pawar <sauravpawar46@gmail.com>
2025-07-22 15:33:42 +0530

    Sort array by descending order using bubble sort logic

---
+++ LogicQuestions_40/SortArray.java
@@ -0,0 +1,22 @@
+package LogicQuestions_40;
+
+public class SortArray {
+  public static void main(String[] args) {
+    int[] arr = { 5, 2, 8, 1, 3 };
+
+    // Bubble sort logic
+
+    for(int i = 0; i<arr.length; i++){
+      for(int j = i+1; j<arr.length; j++){
+        if(arr[j] > arr[i]){
+          int temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
